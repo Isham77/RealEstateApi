0acac33241d064ba4d51f5874f97ffb6b5ef7033
isham <ismail.ihthisham@raasoft.com>
2025-06-17 10:59:13 +0530

    Add project files.

---
+++ RealEstateApi/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Image Url cant be null")]
+        public string ImageUrl { get; set; }
+        public ICollection<Property> Properties { get; set; }
+    }
+}
